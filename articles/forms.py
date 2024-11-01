0515fb1c2229783b3f5162503eabb2b429a5f00f
kngom <kngom@groupeidyal.com>
2024-11-01 14:44:29 +0000

    mise a jour des articles

--- articles/forms.py
+++ articles/forms.py
@@ -5,3 +5,15 @@ class ArticleForm(forms.ModelForm):
       class Meta:
           model = Article
           fields = ['titre','resume','contenu','image','prix_kilo','origine','disponible','quantite_disponible','mode_conservation','temps_livraison']
+          widgets = {
+            'titre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Titre de l\'article'}),
+            'resume': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Résumé'}),
+            'contenu': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Contenu de l\'article'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'prix_kilo': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Prix par kilo'}),
+            'origine': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Origine'}),
+            'disponible': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'quantite_disponible': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Quantité disponible'}),
+            'mode_conservation': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Mode de conservation'}),
+            'temps_livraison': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Temps de livraison en jours'}),
+        }
--- articles/urls.py
+++ articles/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 ##.parcequ ils sont dans le meme repertoire
-from .views import home,useradmin,supprimer_article
+from .views import home,useradmin,supprimer_article,modifier_article
 from django.conf.urls.static import static
 from blog import settings
 # ,newarticle
 urlpatterns = [
     path("",home,name="home"),
     path("useradmin/",useradmin,name="useradmin"),
-    path('supprimer_article/<int:article_id>/',supprimer_article, name='supprimer_article')
-    # path('modifier_article/<int:article_id>/',modifier_article, name='modifier_article')
+    path('supprimer_article/<int:article_id>/',supprimer_article, name='supprimer_article'),
+    path('modifier_article/<int:article_id>/',modifier_article, name='modifier_article')
     # path("newarticle/",newarticle,name="newarticle")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
